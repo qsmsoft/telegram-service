35579beebb165af92f21968fcc119837c33c6f8e
qsmsoft <qsmsoft@gmial.com>
2024-09-04 18:05:23 +0500

    file structure has been updated v2

---
+++ app/models/message.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Text, BigInteger
+
+from app.db.base import Base
+
+
+class Message(Base):
+    __tablename__ = "messages"
+
+    id = Column(Integer, primary_key=True, index=True)
+    sender_id = Column(BigInteger)
+    sender_name = Column(String)
+    receiver_id = Column(BigInteger)
+    receiver_name = Column(String)
+    content = Column(Text)
+    voice_file_path = Column(String, nullable=True)
